d550cdca9b6fa1682dcff38b73044d4483991fe0
sunlazor <nnenter@gmail.com>
2016-06-28 21:00:43 +0300

    Изменение доступа к полям класса Реализация функции ToString() Реализация конструктора по умолчанию

--- genetic-algorithm/Test1Color/Colors/Specimen.cs
+++ genetic-algorithm/Test1Color/Colors/Specimen.cs
@@ -8,89 +8,34 @@ namespace Genetic
         private const double MUTATE_PROBABILITY = 0.1d;
         private const byte MUTATE_INTERVAL = 5;
 
-        private byte color;
-        private double crossoverProbability;
-        private int fit;
+        public byte Color { get; set; }
+        public double CrossoverProbability { get; set; }
+        public int Fit { get; set; }
 
         private static Random random = new Random();
+
+        public Specimen()
+        {
+            Color = Utils.StayInByte(random.Next(75, 175));
+            CrossoverProbability = 0;
+            Fit = 0;
+        }
 
         // Производит мутацию значения "цвета"
         // Пример: 100 и интервал_мутации = 5 -> 98-102
         public void Mutate()
         {
             int mutation = random.Next(MUTATE_INTERVAL) - (MUTATE_INTERVAL / 2);
-            color = Utils.StayInByte(color + mutation);
+            Color = Utils.StayInByte(Color + mutation);
         }
 
+        // Распечатать информацию о классе
         override public string ToString()
         {
-            return "Specimen";
+            string text = "color:" + Color.ToString() + " ";
+            text += "P:" + CrossoverProbability.ToString() + " ";
+            text += "fit:" + Fit.ToString() + "\n";
+            return text;
         }
-
-        //// Распечатать информацию о классе
-        //public string ToString(int num)
-        //{
-        //    string text = "#" + num.ToString() + ": ";
-        //    text += "R:" + color.R.ToString() + " ";
-        //    text += "G:" + color.G.ToString() + " ";
-        //    text += "B:" + color.B.ToString() + " ";
-        //    text += "P:" + crossoverProbability.ToString().PadRight(5) + " ";
-        //    text += "fit:" + fit.ToString() + "\n";
-        //    return text;
-        //}
-
-        //public Specimen(int count)
-        //{
-        //    color = Color.FromArgb(random.Next(120, 135), random.Next(120, 135), random.Next(120, 135));
-        //    crossoverProbability = 1d / (double)count;
-        //    fit = 0;
-        //}
-
-        //// Функции для цвета
-        //public void SetColor(Color color)
-        //{
-        //    this.color = color;
-        //}
-        //public Color GetColor()
-        //{
-        //    return color;
-        //}
-        //public int GetRed()
-        //{
-        //    return color.R;
-        //}
-        //public int GetGreen()
-        //{
-        //    return color.G;
-        //}
-        //public int GetBlue()
-        //{
-        //    return color.B;
-        //}
-
-        //// Функции вероятности скрещивания
-
-        //// Возвращает true в случае удачного присваивания
-        //public bool SetCP(double value)
-        //{
-        //        crossoverProbability = value;
-        //        return true;
-        //}
-
-        //public double GetCP()
-        //{
-        //    return crossoverProbability;
-        //}
-
-        //// Коэффициент соответствия
-        //public void SetFit(int fit)
-        //{
-        //    this.fit = fit;
-        //}
-
-        //public int GetFit()
-        //{
-        //    return fit;
-        //}
     }
 }
