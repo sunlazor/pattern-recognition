716e6e8648aea6e39dca4841b15bbb563c2c123a
sunlazor <nnenter@gmail.com>
2016-06-28 21:02:38 +0300

    Реализация конструкторов Реализация функции подсчета приспособленности Реализация функции получения минимального значения функции приспособленности среди особей поколения

--- genetic-algorithm/Test1Color/Colors/Generation.cs
+++ genetic-algorithm/Test1Color/Colors/Generation.cs
@@ -7,24 +7,93 @@ namespace Genetic
 {
     class Generation
     {
-        List<Specimen> generation;
+        public readonly byte GoalColor;
 
+        private const int DEFAULT_SIZE = 10;
+
+        private List<Specimen> generation;
+        
+        private int Size
+        {
+            get
+            {
+                return generation.Count;
+            }
+        }
         // Начальное поколение
-        public Generation()
-        { }
+        public Generation() : this(DEFAULT_SIZE)
+        {
+        }
+
+        public Generation(int size, byte goal = byte.MaxValue)
+        {
+            if (size <= 0 || size >= int.MaxValue)
+            {
+                size = DEFAULT_SIZE;
+            }
+            else if (size % 2 != 0)
+            {
+                size += 1;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                generation.Add(new Specimen());
+            }
+            GoalColor = goal;
+        }
 
         // Последующие поколения
-        public Generation(Generation previous)
-        { }
+        public Generation(Generation previous) : this(previous.Size, previous.GoalColor)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                generation[i].Color = previous.generation[i].Color;
+                generation[i].CrossoverProbability = previous.generation[i].CrossoverProbability;
+                generation[i].Fit = previous.generation[i].Fit;
+            }
+        }
 
+        // Скрещивание особей
         void Crossover()
-        { }
+        {
+            for (int i = 0; i < Size; i += 2)
+            {
+                byte value1 = generation[i].Color;
+                byte value2 = generation[i + 1].Color;
+
+                int tmp1 = value1 & 240; // 11110000
+                int tmp2 = value2 & 31; // 00001111
+                int res = tmp1 | tmp2;
+                generation[i].Color = Utils.StayInByte(res);
+
+                tmp1 = value2 & 240; // 11110000
+                tmp2 = value1 & 31; // 00001111
+                res = tmp1 | tmp2;
+                generation[i + 1].Color = Utils.StayInByte(res);
+            }
+        }
 
         public void CalculateFitness()
-        { }
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                generation[i].Fit = Math.Abs(GoalColor - generation[i].Color);
+            }
+        }
 
-        public void CheckFitness()
-        { }
+        // Возвращает минимальное значение приспособленности 
+        public int MinFitness()
+        {
+            int minFit = int.MaxValue;
+            for (int i = 0; i < Size; i++)
+            {
+                if (minFit > generation[i].Fit)
+                {
+                    minFit = generation[i].Fit;
+                }
+            }
+            return minFit;
+        }
 
         public void CalculateCrossoverProbability()
         { }
