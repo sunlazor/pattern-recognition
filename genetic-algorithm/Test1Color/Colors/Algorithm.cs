1e8f9a34352d843faf43985ce953f184cde72773
sunlazor <nnenter@gmail.com>
2016-06-29 16:29:44 +0300

    Реализация функции вывода информации о поколении внутри работы алгоритма Реализация главной функции работы алгоритма

--- genetic-algorithm/Test1Color/Colors/Algorithm.cs
+++ genetic-algorithm/Test1Color/Colors/Algorithm.cs
@@ -7,8 +7,41 @@ namespace Genetic
 {
     class Algorithm
     {
+        private const int GENERATION_COUNT = 3000;
+        private const int GENERATION_SIZE = 10;
+
+        private int currentGenerationNumber = 0;
+
+        Generation currentGeneration;
+
+        private string PrintCurrentGeneration()
+        {
+            string result = "";
+            result += "Current generation: #" + currentGenerationNumber.ToString() +"\n";
+            result += currentGeneration.ToString();
+            return result;
+        }
+
         // Главная функция работы алгоритма
         public int Work()
-        { return 0; }
+        {
+            for (currentGenerationNumber = 0; currentGenerationNumber < GENERATION_COUNT; currentGenerationNumber++)
+            {
+                if (currentGeneration == null)
+                    currentGeneration = new Generation(GENERATION_SIZE);
+                else
+                    currentGeneration = new Generation(currentGeneration);
+                currentGeneration.CalculateFitness();
+                Console.Write(PrintCurrentGeneration());
+                if (currentGeneration.MinFitness() == 0)
+                {
+                    return currentGenerationNumber;
+                }
+                currentGeneration.Crossover();
+                currentGeneration.Mutate();
+                currentGeneration.CalculateCrossoverProbability();
+            }
+            return 0;
+        }
     }
 }
