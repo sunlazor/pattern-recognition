a7a43215c7c08707ff653f2c7e1c2ac3e78f15e2
sunlazor <nnenter@gmail.com>
2016-06-30 12:02:14 +0300

    Создание класса нейрона Создание первой итерации нейронной сети

---
+++ genetic-algorithm/Test1Color/Neural/Network.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neural
+{
+    class Network
+    {
+        // Нейроны
+        List<Neuron> neurons;
+        // Количество нейронов в сети
+        private int NeuronCount { get; set; }
+        // Количество входов нейронов
+        private int InputCount { get; set; }
+
+        public Network(int neuronCount, int neuronInputCount)
+        {
+            NeuronCount = neuronCount;
+            InputCount = neuronInputCount;
+            neurons = new List<Neuron>(NeuronCount);
+            for (int i = 0; i < NeuronCount; ++i)
+            {
+                neurons.Add(new Neuron(InputCount));
+            }
+        }
+    }
+}
---
+++ genetic-algorithm/Test1Color/Neural/Neuron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neural
+{
+    class Neuron
+    {
+        int InputCount { get; set; }
+
+        static Random random = new Random();
+
+        double[] x;   // Значение входа нейрона
+        double[] w; // Веса входов
+        double y; // Выходы
+        double sum; // Значащая сумма
+        double h;   // хз
+        double w0;  // хз, дополнительный вес
+
+        public Neuron(int inputCount)
+        {
+            InputCount = inputCount;
+            x = new double[InputCount];
+            w = new double[InputCount];
+            for (int i = 0; i < InputCount; i++)
+            {
+                x[i] = 0;
+                w[i] = random.NextDouble() - 0.5d;
+            }
+            y = 0;
+            sum = 0;
+            h = 1;
+            w0 = 1;
+        }
+    }
+}
