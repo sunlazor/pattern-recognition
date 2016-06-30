0db18824c903923864138c1b6da5df79bd8e88b9
sunlazor <nnenter@gmail.com>
2016-06-30 15:04:18 +0300

    Реализация функции обучения Исправление функции Подсчета выхода Сети, на выход Нейрона Реализация главной функции работы сети

--- genetic-algorithm/Test1Color/Neural/Network.cs
+++ genetic-algorithm/Test1Color/Neural/Network.cs
@@ -26,20 +26,64 @@ namespace Neural
         }
 
         // Расчет для каждого нейрона
-        void CalculateNeurons()
+        void CalculateNeuron(int neuronNumber)
         {
             double Net = 0;// взвешенная сумма входных сигналов,
-            for (int j = 0; j < neurons.Count; j++)
+            for (int i = 0; i < InputCount; ++i)
             {
-                for (int i = 0; i < InputCount; ++i)
-                {
-                    // Функция взвешенной  суммы (сумматор)
-                    Net += (neurons[j].x[i] - 0.5/*В случае бинарных входов нужно 0.5*/) * neurons[j].w[i];
-                }
-                Net += neurons[j].w0;
-                neurons[j].sum = Net;
-                neurons[j].y = Activation(Net);
+                // Функция взвешенной  суммы (сумматор)
+                Net += (neurons[neuronNumber].x[i] - 0.5) * neurons[neuronNumber].w[i];
             }
+            Net += neurons[neuronNumber].w0;
+            neurons[neuronNumber].sum = Net;
+            neurons[neuronNumber].y = Activation(Net);
+        }
+
+        // Определние образа
+        // Возвращает номер нейрона  с макс выходом
+        public int Work()
+        {
+            double Max = 0;
+            int Num = 0;
+            for (int i = 0; i < NeuronCount; i++)
+            {
+                CalculateNeuron(i);
+                switch (i)
+                {
+                    case 0: Console.WriteLine(neurons[0].y); break;
+                    case 1: Console.WriteLine(neurons[1].y); break;
+                    case 2: Console.WriteLine(neurons[2].y); break;
+                    case 3: Console.WriteLine(neurons[3].y); break;
+                    default: break;
+                }
+                if (neurons[i].y > Max)
+                {
+                    Max = neurons[i].y;
+                    Num = i;
+                }
+            }
+            return Num;
+        }
+
+        // Num - номер нейрона
+        public int Teach(int Num)
+        {
+            // Расчет весов
+            Work();
+            for (int i = 0; i < NeuronCount; ++i)
+            {
+                double t = 0;
+                if (Num == i)
+                    t = 1;
+                double deltaRes = neurons[i].y - t;
+                double delta = ActivationDerevative(neurons[i].sum) * deltaRes;
+                for (int j = 0; j < InputCount; j++)
+                {
+                    neurons[i].w[j] = neurons[i].w[j] - neurons[i].h * delta * neurons[i].x[j];
+                }
+                neurons[i].w0 = neurons[i].w0 - neurons[i].h * delta;
+            }
+            return 0;
         }
 
         public static double Activation(double x)
