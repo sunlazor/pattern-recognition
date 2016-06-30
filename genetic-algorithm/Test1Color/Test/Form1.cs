3037fc2f1bb1d9d74c1aa8a3614c33ff98057df1
sunlazor <nnenter@gmail.com>
2016-06-30 11:02:13 +0300

    Добавление начального проекта нейронной сети Добавление проекта для тестирования кусков кода

---
+++ genetic-algorithm/Test1Color/Test/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class testclass
+        {
+            int a;
+
+            Bi ccc;
+
+           
+
+            public void gogog()
+            {
+                if (ccc == null)
+                {
+                    Console.WriteLine("ccc = null");
+                }
+            }
+        }
+
+        class Bi
+        {
+            int b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Bi b;
+            //if (b == null)
+            //{
+            //    Console.WriteLine("ccc = null");
+            //}
+
+            testclass a = new testclass();
+            a.gogog();
+        }
+    }
+}
