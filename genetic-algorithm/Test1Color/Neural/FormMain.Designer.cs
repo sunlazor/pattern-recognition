9df22fa3eb3636596379224692ceb0c4e47989d8
sunlazor <nnenter@gmail.com>
2016-06-30 12:25:10 +0300

    Дополнительные файлики относящиеся к переименованию файла главной формы

--- genetic-algorithm/Test1Color/Neural/FormMain.Designer.cs
+++ genetic-algorithm/Test1Color/Neural/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Neural
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btBlack = new System.Windows.Forms.Button();
+            this.btWhite = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btWhite);
+            this.groupBox1.Controls.Add(this.btBlack);
+            this.groupBox1.Location = new System.Drawing.Point(45, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(94, 91);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подать";
+            // 
+            // btBlack
+            // 
+            this.btBlack.Location = new System.Drawing.Point(6, 19);
+            this.btBlack.Name = "btBlack";
+            this.btBlack.Size = new System.Drawing.Size(75, 23);
+            this.btBlack.TabIndex = 0;
+            this.btBlack.Text = "Черный";
+            this.btBlack.UseVisualStyleBackColor = true;
+            // 
+            // btWhite
+            // 
+            this.btWhite.Location = new System.Drawing.Point(6, 48);
+            this.btWhite.Name = "btWhite";
+            this.btWhite.Size = new System.Drawing.Size(75, 23);
+            this.btWhite.TabIndex = 1;
+            this.btWhite.Text = "Белый";
+            this.btWhite.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Подан:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(221, 157);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormMain";
+            this.Text = "Нейронная сеть - Распознование цвета";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btWhite;
+        private System.Windows.Forms.Button btBlack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
